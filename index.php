1692e45f824444289d21f10b84c7527d50624a55
MrChrootInside <40430338+MrChrootInside@users.noreply.github.com>
2019-01-01 10:50:56 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<html>
+
+<head>
+
+<title>Hacked By MR.Caplin</title>
+
+</head>
+
+<body bgcolor=black>
+
+<center><img witdh=300 height=300 src="https://metexpo.in/20180914_122024.png"></center>
+
+<center><h2><font color=red>Hacked By MR.Caplin</font></center></h2>
+
+<h1><center><font color=red>"Sebuah Hari Tanpa Tertawa</font></h1>
+
+<h1><center><font color=red>Adalah Hari Yang TIDAK BERGUNA"</font></h1>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<marquee scrollamount=150><font color=red>--------------------------------</marquee></font>
+
+<center><marquee scrollamount=10><font color=red>Bye Admin</center></marquee></font>
+
+<marquee scrollamount=150 direction=right><font color=red>--------------------------------</marquee></font>
+
+</body>
+
+</html>
